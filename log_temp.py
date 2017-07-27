bc1d81b6b5fde8211e355823dc142971a2d8d755
michaeldaas <daas@physik.uni-bonn.de>
2017-07-27 11:12:32 +0200

    Add simple example for MCP9808 temp sensor

---
+++ log_temp.py
@@ -0,0 +1,13 @@
+import time
+import datetime
+import Adafruit_MCP9808.MCP9808 as MCP9808
+
+sensor = MCP9808.MCP9808()
+sensor.begin()
+
+while True:
+	temp = str(sensor.readTempC())
+	with open('temp.log', 'a') as outfile:
+		outfile.write(datetime.datetime.now().isoformat(' ') + ',	' + temp + '\n')
+	print datetime.datetime.now().isoformat(' ') + ' 	' + temp
+	time.sleep(60)
