79fd7fdc3ff59633bc994e6a76ccfff545ef6435
silab <silab@silab-PC>
2017-08-24 11:16:58 +0200

    ENH: support multiple sensors

--- log_temp.py
+++ log_temp.py
@@ -1,16 +1,44 @@
 import time
 import datetime
+import logging
 
 # https://github.com/adafruit/Adafruit_Python_MCP9808
 import Adafruit_MCP9808.MCP9808 as MCP9808
 
-sensor = MCP9808.MCP9808(address=0x18)
-sensor.begin()
 
-while True:
-    temp = str(sensor.readTempC())
+def get_sensors(adresses):
+    sensors = []
+    for address in addresses:
+        try:
+            sensor = MCP9808.MCP9808(address)
+            sensor.begin(sensor)
+            sensors.append()
+        except IOError:
+            logging.warning("Device at address %d not available", address)
+    return sensors
+
+
+def read_sensors(sensors, delay=1):
+    """ Read the sensors and add to file.
+    Delay readings in seconds
+    """
     with open('temp.log', 'a') as outfile:
-        outfile.write(datetime.datetime.now().isoformat(
-            ' ') + ',	' + temp + '\n')
-    print datetime.datetime.now().isoformat(' ') + ' 	' + temp
-    time.sleep(1)
+        s_str = [sensor.get_address() for sensor in sensors]
+        s_str = '\t'.join(s_str)
+        outfile.write("\t" + s_str)
+    while True:
+        temps = []
+        for sensor in sensors:
+            temps.append(str(sensor.readTempC()))
+        with open('temp.log', 'a') as outfile:
+            outfile.write(datetime.datetime.now().isoformat(
+                ' ') + '\t'.join(temps) + '\n')
+            logging.info(datetime.datetime.now().isoformat(
+                ' ') + '%s', '\t'.join(temps))
+        time.sleep(delay)
+
+
+if __name__ == "__main__":
+    addresses = [0x18, 0x19, 0x1a]
+    sensors = get_sensors(addresses)
+    read_sensors(sensors, delay=1)
