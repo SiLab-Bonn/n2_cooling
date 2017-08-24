dea9212e7285192bc797b461814acead01b2199a
silab <silab@silab-PC>
2017-08-24 11:02:12 +0200

    ENH: try adderessing

--- log_temp.py
+++ log_temp.py
@@ -4,12 +4,13 @@ import datetime
 # https://github.com/adafruit/Adafruit_Python_MCP9808
 import Adafruit_MCP9808.MCP9808 as MCP9808
 
-sensor = MCP9808.MCP9808()
+sensor = MCP9808.MCP9808(address=0x18)
 sensor.begin()
 
 while True:
-	temp = str(sensor.readTempC())
-	with open('temp.log', 'a') as outfile:
-		outfile.write(datetime.datetime.now().isoformat(' ') + ',	' + temp + '\n')
-	print datetime.datetime.now().isoformat(' ') + ' 	' + temp
-	time.sleep(1)
+    temp = str(sensor.readTempC())
+    with open('temp.log', 'a') as outfile:
+        outfile.write(datetime.datetime.now().isoformat(
+            ' ') + ',	' + temp + '\n')
+    print datetime.datetime.now().isoformat(' ') + ' 	' + temp
+    time.sleep(1)
