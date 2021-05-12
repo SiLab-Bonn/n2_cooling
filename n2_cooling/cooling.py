087229a07e9d5ea5cbfa01add12c03970b4ff402
Christian Bespin <bespin@physik.uni-bonn.de>
2021-05-12 18:10:47 +0200

    ENH: Major cleanup, log values to h5 file

--- n2_cooling/cooling.py
+++ n2_cooling/cooling.py
@@ -2,11 +2,15 @@ import time
 import zmq
 import logging
 import numpy as np
+import tables as tb
+
+from simple_pid import PID
 
 from basil.dut import Dut
 from online_monitor.utils import utils
 
-FORMAT = '%(asctime)s [%(name)-20s] - %(levelname)-7s %(message)s'
+FORMAT = '%(asctime)s [%(name)-15s] - %(levelname)-7s %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
 
 
 def send_data(socket, data, name="CoolingData"):
@@ -24,22 +28,11 @@ def send_data(socket, data, name="CoolingData"):
         pass
 
 
-def print_log(logstring,logfile="202008_DESY_temperature.log"):
-    with open(logfile,"a") as f:
-        f.write("%s %s\n"%(time.ctime(),logstring))
-    print(logstring)
-
-##### For the valve: open/close max(open)=0xFFFFFF, min(close)=0
-
-
-##### monitor with plot
 class Cooling(object):
     def __init__(self, conf_file="../examples/cooling.yaml", setpoint=-20, monitor=False):
         # Setup logging
         self.log = logging.getLogger('N2 Cooling')
-        self.log.setLevel(logging.INFO)
-        self.log.setFormatter(logging.Formatter(FORMAT))
-        fh = logging.FileHandler('cooling.log')
+        fh = logging.FileHandler('cooling_2021-05-12_2e15.log')
         fh.setLevel(logging.INFO)
         fh.setFormatter(logging.Formatter(FORMAT))
         self.log.addHandler(fh)
@@ -49,79 +42,125 @@ class Cooling(object):
             try:
                 context = zmq.Context()
                 self.socket = context.socket(zmq.PUB)
-                self.socket.bind(args.monitor)
-                self.log.info("Sending data to server %s" % args.monitor)
+                self.socket.bind(monitor)
+                self.log.info("Sending data to server %s" % monitor)
             except zmq.error.ZMQError:
                 self.log.warning("Cannot connect to socket for data sending")
                 self.socket = None
         else:
             self.socket = None
-           
+
+        # Set up temperature log file
+        self.temp_type = np.dtype([('timestamp', 'u4'), ('temp_1', 'f4'), ('temp_2', 'f4'), ('humidity_1', 'f4'), ('humidity_2', 'f4')])
+        self.output_file = tb.open_file('cooling.h5', 'a')
+        if '/temperature' in self.output_file:  # Table already exists
+            self.temp_table = self.output_file.root.temperature
+        else:
+            self.temp_table = self.output_file.create_table(self.output_file.root,
+                                                            name='temperature',
+                                                            description=self.temp_type,
+                                                            filters=tb.Filters(complevel=5, complib='blosc')
+                                                            )            
+
         self.setpoint = setpoint
         devices = Dut(conf_file)
         devices.init()
         self.temp_sensors = devices["sensirion"]
         self.valve = devices["bronkhorst"]
 
-    def run(self, valve=0x000000, feedback="on"):
-        if feedback== "open":
-            mode=8
-        elif feedback=="close":
-            mode=3
-        else: #"on"
-            mode=4
-        self.valve.set_control_mode(mode)
-        ret = self.valve.get_control_mode()
-        if ret == mode:
-            self.log.info("Valve started with valve %s" % feedback)
-        else:
-            self.log.error("ERR: Control mode cannot be set, mode is %s" % str(ret))
+    def __del__(self):
+        self.output_file.close()
 
-        interval=10
-        #### init buffers
-        temp = np.ones(10000)*float("NaN")
-        i=0
+    def get_temps(self):
+        temps = self.temp_sensors.get_temperature()
+        while not all(temps[:2]):  # Make sure to get proper values for sensor 1 and 2
+            temps = self.temp_sensors.get_temperature()
+            time.sleep(0.1)
+        time.sleep(0.2)
+        try:
+            humids = self.temp_sensors.get_humidity()
+            self.log.info("%.2f C %.2f % RH" % (temps[0], humids[0]))
+        except Exception:
+            humids = [np.nan for _ in range(4)]
+            self.log.warning("%.2f C Humidity read error" % (temps[0]))
+
+        # Log to file
+        self.temp_table.append([(int(time.time()), temps[0], temps[1], humids[0], humids[1])])
+        self.temp_table.flush()
+
+        return temps, humids
+
+    def open_valve(self, valve=0xFFFFFF):
+        self.valve.set_control_mode(8)
+        if self.valve.get_control_mode() == 8:
+            self.valve.set_valve_output(valve)
+            while True:
+                temps, humids = self.get_temps()
+                if self.socket:
+                    send_data(self.socket, data=np.array([temps, humids], dtype=np.float64))
+                time.sleep(1)
+        else:
+            self.log.error("Control mode cannot be set!")
+
+    def close_valve(self, valve=0x0):
+        self.valve.set_control_mode(3)
+        if self.valve.get_control_mode() == 3:
+            self.valve.set_valve_output(valve)
+            while True:
+                temps, humids = self.get_temps()
+                if self.socket:
+                    send_data(self.socket, data=np.array([temps, humids], dtype=np.float64))
+                time.sleep(1)
+        else:
+            self.log.error("Control mode cannot be set!")
+
+    def run(self, valve=0x000000):
+        self.valve.set_control_mode(4)
+        ret = self.valve.get_control_mode()
+        if ret == 4:
+            self.log.info("Starting...")
+        else:
+            self.log.error("Control mode cannot be set, mode is %s" % str(ret))
+
+        # Init buffers
+        temp = np.ones(10000) * float("NaN")
+        i = 0
+        # pid = PID(0.7, 0.001, 0, setpoint=-20, sample_time=.5)
+        # pid.output_limits = (0, 1)
 
         self.valve.set_valve_output(0x000000)
-        if mode == 4:
-            self.valve.set_valve_output(valve)
-        time.sleep(1)
+        time.sleep(2)
         self.log.info("valve 0x%x,0x%x" % (self.valve.get_valve_output(), self.valve.get_measure()))
         pre = self.temp_sensors.get_temperature()[0]
         flg = 0
         while True:
-            temps = self.temp_sensors.get_temperature()
-            temp[i % len(temp)] = temps[0]
-            try:
-                humids = self.temp_sensors.get_humidity()
-                self.log.info("%.2fC %.2f RH" % (temp[i % len(temp)], humids[0]))
-            except:
-                print_log("%.2fC Humidity read error" % (temp[i%len(temp)]),logfile=logfile)
-
-            if socket:
-                send_data(socket, data=np.array([temps, humids], dtype=np.float64))
-
+            temps, humids = self.get_temps()
+            if self.socket:
+                send_data(self.socket, data=np.array([temps, humids], dtype=np.float64))
             time.sleep(1)
 
+            # valve = 6500000 + (10000000 - 6500000) * pid(temps[0])
+            # print("valve: ", int(valve))
+            # self.valve.set_valve_output(int(valve))
+
+            temp[i % len(temp)] = temps[0]
+
             if flg > 1:
-                flg=flg-1
-            elif flg==1 and temp[i%len(temp)] > setpoint-0.5 and self.valve.get_valve_output()!=0 and mode==4:
+                flg = flg - 1
+            elif flg == 1 and temp[i % len(temp)] > self.setpoint - 0.5 and self.valve.get_valve_output() != 0:
                 self.valve.set_valve_output(0x000000)
-                print_log("valve 0x%x,0x%x"%(self.valve.get_valve_output(),self.valve.get_measure()),
-                          logfile=logfile)
+                self.log.info("valve 0x%x,0x%x" % (self.valve.get_valve_output(), self.valve.get_measure()))
                 flg = 20 
-            elif temp[i%len(temp)] < setpoint-0.2 and self.valve.get_valve_output()==0 and mode==4:
+            elif temp[i % len(temp)] < self.setpoint - 0.2 and self.valve.get_valve_output()==0:
                 self.valve.set_valve_output(0x800000)  # open valve, was originally 0x800000
                 flg = 10
-                print_log("valve 0x%x,0x%x"%(self.valve.get_valve_output(),self.valve.get_measure()),
-                          logfile=logfile)
-                pre=temp[i%len(temp)]
-            elif temp[i%len(temp)] > setpoint and self.valve.get_valve_output()!=0 and mode==4:
+                self.log.info("valve 0x%x,0x%x" % (self.valve.get_valve_output(), self.valve.get_measure()))
+                pre=temp[i %len(temp)]
+            elif temp[i % len(temp)] > self.setpoint and self.valve.get_valve_output() != 0:
                 self.valve.set_valve_output(0x000000)
                 flg = 50
-                print_log("valve 0x%x,0x%x"%(self.valve.get_valve_output(),self.valve.get_measure()),
-                          logfile=logfile)
-            i = i + 1
+                self.log.info("valve 0x%x,0x%x" % (self.valve.get_valve_output(), self.valve.get_measure()))
+            i += 1
 
 
 if __name__ == "__main__":
@@ -131,13 +170,11 @@ if __name__ == "__main__":
         formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('--setpoint', type=float, default=-20.0,
                         help="Target temperature in Celsius")
-    parser.add_argument('--feedback', type=str, default="on",
-                        help="on: feedback on, open: open completely, close: close completely")
-    parser.add_argument('--logfile', type=str, default="2020-12_DESY_temperature.log",
-                        help="log file")
-    parser.add_argument('--monitor', type=str, default="tcp://127.0.0.1:5000", help="Online monitor address including port")
+    parser.add_argument('--logfile', type=str, default="temperature.log",
+                        help="Filename for log file")
+    parser.add_argument('--monitor', type=str, default="tcp://127.0.0.1:5000",
+                        help="Online monitor address including port")
     args = parser.parse_args()
 
-    cooling = Cooling(setpoint=setpoint, monitor=monitor)
+    cooling = Cooling(setpoint=args.setpoint, monitor=args.monitor)
     cooling.run()
-
