b1ffe74adb3023d0153d0179f2b56df58786a31c
Houdaifa Lahbil <houdaifa_lahbil@gmx.de>
2023-02-02 13:46:25 +0100

    ENH: make setup.py work

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ import n2_cooling
 
 author = 'Christian Bespin, Toko Hirono'
 author_email = ''
+version = '0.1.0'
 
 # Requirements
 install_requires = ['basil-daq', 'numpy', 'online_monitor', 'simple-pid', 'tables', 'pyzmq']
 
 setup(
     name='n2cooling',
-    version=n2_cooling.version,
+    version=version,
     description='Control software for N2 cooling system',
     url='https://github.com/SiLab-Bonn/n2_cooling',
     license='',
@@ -23,7 +24,7 @@ setup(
     author_email=author_email,
     maintainer_email=author_email,
     install_requires=install_requires,
-    python_requires=">=3.0",
+    python_requires=">=3.7",
     packages=find_packages(),
     include_package_data=True,
     platforms='any',
